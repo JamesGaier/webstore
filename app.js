14c5bfc5fcb03bcb552893eed86ee2dd5b9a5cad
James <james.gaier003@gmail.com>
2019-11-28 01:31:19 -0800

    first commit

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const url = require("url");
+var mysql = require('mysql');
 const app = express();
 const port = 3000;
+
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "123",
+  database: "mydb"
+});
+
+con.connect();
+
 
 app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(bodyParser());
@@ -14,20 +27,38 @@ app.set("view engine", "ejs");
 
 
 app.get("/", (req, res) => {
-    res.render("view", {
-       people: [
-            {name: "dave"},
-            {name: "jerry"}
-        ]
+    con.query("SELECT * FROM dress_info;",(err,result) => {
+        if(err) throw err;
+
+        let arr = [];
+        let dress_info = [];
+        for(let i = 0; i < result.length; i++) {
+            arr.push(result[i]);
+            if(i == 3 || i == result.length - 1) {
+                dress_info.push(arr);
+                arr = []
+            }
+
+        }
+        res.render("index", {items: dress_info});
+
     });
 });
+
+
 app.get("/item", (req, res) => {
-    res.render("item", {
-       dress: {
-            name: "blah",
-            url: "public/images/1.png"
-        }
+    const queryObject = url.parse(req.url, true).query;
+    con.query("SELECT * FROM dress_info WHERE name=?",queryObject.n, (err, result) => {
+        if(err) throw err;
+        res.render("item", {
+           dress: {
+                src: result[0].src,
+                name: result[0].name,
+                price: result[0].price
+            }
+        });
     });
+    
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
